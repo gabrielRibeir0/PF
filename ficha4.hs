4e7263edc38ce3fabf33355765c294e725919c86
Gabriel Ribeiro <a104171@alunos.uminho.pt>
2022-11-16 16:03:14 +0000

    complete ficha4.hs

--- ficha4.hs
+++ ficha4.hs
@@ -70,7 +70,65 @@ fromDigitsAc' ac (h:t) = fromDigitsAc' (h+ac*10) t
 --5)função auxiliar com acumuladores, optimize a definição que determina a soma do segmento inicial de uma lista com soma máxima
 {-maxSumInit :: (Num a, Ord a) => [a] -> a
 maxSumInit l = maximum [sum m | m <- inits l]-}
+maxSumInit :: (Num a, Ord a) => [a] -> a
+maxSumInit l = maxSumInitAux l (sum l)
 
+maxSumInitAux :: (Num a, Ord a) => [a] -> a -> a
+maxSumInitAux [] acc = acc
+maxSumInitAux l acc = if si > acc then maxSumInitAux il si else maxSumInitAux il acc
+    where il = init l
+          si = sum il
+
+--6)função auxiliar com acumuladores para otimizar a função que calcula o n-ésimo número da seq. de Fibonacci
+{-fib :: Int -> Int
+fib 0 = 0
+fib 1 = 1
+fib n = fib (n-1) + fib (n-2)-}
+fib :: Int -> Int
+fib 0 = 0
+fib 1 = 1
+fib n = fibAux (n-2) 0 1
+
+fibAux :: Int -> Int -> Int -> Int
+fibAux 0 n _ = n
+fibAux i fib_n fib_n_mais_1 = fibAux (i - 1) fib_n_mais_1 (fib_n + fib_n_mais_1)
+
+--7)função auxiliar com aucumuladores para a função que converte um inteiro numa string
+intToStr :: Integer -> String
+intToStr 0 = "zero"
+intToStr n = intToStrAux n ""
+
+intToStrAux :: Integer -> String -> String
+intToStrAux 0 ('-':acc) = acc
+intToStrAux n acc = intToStrAux nn ((case r of 
+        0 -> "-zero"
+        1 -> "-um"
+        2 -> "-dois"
+        3 -> "-três"
+        4 -> "-quatro"
+        5 -> "-cinco"
+        6 -> "-seis"
+        7 -> "-sete"
+        8 -> "-oito"
+        9 -> "-nove") ++ acc)
+    where (nn,r) = n `divMod` 10 
+
+--8)Enumerar as listas seguintes e apresentar uma forma alternativa de as obter
+--a)[x | x <- [1..20], mod x 2 == 0, mod x 3 == 0]
+l8a = [6,12,18]
+l8a2 = [x | x <- [1..20], mod x 6 == 0]
+
+--b)[x | x <- [y | y <- [1..20], mod y 2 == 0], mod x 3 == 0]
+l8b = [6,12,18]
+l8b2 = [x | x <- [1..20], mod x 6 == 0]
+
+--c)[(x,y) | x <- [0..20], y <- [0..20], x+y == 30]
+l8c = [(10,20),(11,19),(12,18),(13,17),(14,16),(15,15),(16,14),(17,13),(18,12),(19,11),(20,10)]
+l8c2 = [(x, 30 - x) | x <- [10..20]]
+
+--d)[sum [y | y <- [1..x], odd y] | x <- [1..10]]
+l8d = [1,1,4,4,9,9,16,16,25,25]
+l8d2 = [ x^2 | x <- [1..5], y <- [1..2]]
 
 --9)definir as listas por compreensão
 --a) [1,2,4,8,16,32,64,128,256,512,1024]
@@ -87,4 +145,5 @@ e9c = [[1..x]| x<-[1..5]]
 e9d = [replicate x 1 | x<- [1..5]]
 e9d' = [take n (repeat 1) | n <- [1..5]]
 
-
+--e)[1,2,6,24,120,720]
+e9e = [product [y | y <- [1..x]] | x <- [1..6]]
