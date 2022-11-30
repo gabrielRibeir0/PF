f5d2461d28e303ba7a9004849e02b248186b33e8
Gabriel Ribeiro <a104171@alunos.uminho.pt>
2022-11-30 17:58:19 +0000

    ficha 7 (quase) feita

--- ficha7.hs
+++ ficha7.hs
@@ -51,15 +51,52 @@ altura (R x l) = maximum(map altura l)
 
 --c)função que remove de uma árvore todos os elementos a partir de uma dada profundidade
 prune :: Int -> RTree a -> RTree a
-prune 0 _ = []
-prune _ [] = []
+prune 0 (R e es) = R e []
+prune n (R e es) = R e (map (prune (n - 1)) es)
 
 --d)função que gera a árvore simátrica
 mirror :: RTree a -> RTree a
+mirror (R e es) = R e (map mirror (reverse es))
 
 --e)função que faz a travessia postorder da árvore (esquerda, direita e depois raiz)
 
---3)tipo de dados para Leaf Trees
+--3)tipo de dados para Leaf Trees (a informação está apenas nas extremidades)
 data LTree a = Tip a | Fork (LTree a) (LTree a)
 
+--a)função que soma as folhas de uma árvore
+ltSum :: Num a => LTree a -> a
+ltSum (Tip f) = f
+ltSum (Fork e d) = ltSum e + ltSum d
 
+--b)função que lista as folhas de uma árvore (da esquerda para a direita)
+listaLT :: LTree a -> [a]
+listaLT (Tip f) = [f]
+listaLT (Fork e d) = listaLT e ++ listaLT d
+
+--c)função que calcula a altura de uma árvore
+ltHeight ::LTree a -> Int
+ltHeight (Tip f) = 0
+ltHeight (Fork e d) = 1 + max (ltHeight e) (ltHeight d)
+
+--4)tipo de dados para representar Full Trees (a informação não está só nos nodos mas também nas folhas, não tendo de ser do mesmo tipo)
+data FTree a b = Leaf b | No a (FTree a b) (FTree a b)
+
+--árvores binárias
+data BTree a = Empty
+             | Node a (BTree a) (BTree a)
+             deriving Show
+
+--a)função que separa uma full tree em duas árvores de tipos diferentes
+splitFTree :: FTree a b -> (BTree a, LTree b)
+splitFTree (Leaf f) = (Empty, Tip f)
+splitFTree (No x e d) = (Node x eb db, Fork el dl)
+    where (eb, el) = splitFTree e
+          (db, dl) = splitFTree d 
+
+--b)função que junta árvores numa só sempre que sejam compatíveis
+joinTrees :: BTree a -> LTree b -> Maybe (FTree a b)
+joinTrees (Empty) (Tip f) = Just (Leaf f)
+joinTrees (Node i e d) (Fork a b) =
+    case (joinTrees e a, joinTrees d b) of (Just x, Just y) -> Just (No i x y)
+                                           _ -> Nothing
+joinTrees _ _ = Nothing
