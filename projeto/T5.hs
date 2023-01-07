f4d2395b030d8600c76384e5327e22356dd8cca9
Gabriel Ribeiro <a104171@alunos.uminho.pt>
2023-01-07 13:38:15 +0000

    apagar o projeto

--- projeto/T5.hs
+++
@@ -1,6 +0,0 @@
-module T5 where
-import LI
-import T2
-
-deslizaJogo :: Int -> Jogo -> Jogo
-deslizaJogo i (Jogo(Jogador (x,y)) (Mapa l ll)) = Jogo (Jogador (x,y+1)) (estendeMapa (Mapa l (init ll)) i)
